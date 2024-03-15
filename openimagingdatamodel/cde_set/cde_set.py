b55f6ab1200b3739a4b4c58efcb38981b96cb9de
Tarik Alkasab <talkasab@partners.org>
2024-03-15 17:16:31 -0400

    Updates to CDE Set info.

--- openimagingdatamodel/cde_set/cde_set.py
+++ openimagingdatamodel/cde_set/cde_set.py
@@ -1,41 +1,50 @@
 """Represents a CDE Set with its component Elements."""
 
-from pydantic import BaseModel, Field, HttpUrl
-from typing import List, Optional, Union, Array
-import requests
 from enum import Enum
+from typing import Array, List, Optional, Union
+
+import requests
+from pydantic import BaseModel, Field, HttpUrl
+
 
 # The order of classes below is based off the order of imports from the cdeSet.ts file
 class Specialty(BaseModel): ...
 
+
 class Version(BaseModel): ...
+
 
 class Event(BaseModel): ...
 
+
 class Organization(BaseModel): ...
+
 
 class Person(BaseModel): ...
 
+
 class Authors(BaseModel): ...
+
 
 class Reference(BaseModel): ...
 
 
-
-
 class CDEElement(BaseModel): ...
+
 
 # These classes correspond to the indexCode.ts file in OIDM
 class SystemEnum(str, Enum):
-    RADLEX = 'RADLEX'
-    SNOMEDCT = 'SNOMEDCT'
-    LOINC = 'LOINC'
+    RADLEX = "RADLEX"
+    SNOMEDCT = "SNOMEDCT"
+    LOINC = "LOINC"
+
 
 class IndexCodeData(BaseModel):
     system: SystemEnum
     code: str
     display: Optional[str] = None
     href: Optional[HttpUrl] = None
+
 
 class IndexCode:
     def __init__(self, in_data: IndexCodeData):
@@ -63,6 +72,7 @@ class BodyPartData(BaseModel):
     name: str
     index_codes: Optional[IndexCodeData] = None
 
+
 class BodyPart:
     def __init__(self, in_data: BodyPartData):
         self._data = in_data
@@ -77,7 +87,6 @@ class BodyPart:
         return self._indexCode
 
 
-
 class Status(BaseModel): ...
 
 
@@ -85,13 +94,13 @@ class Status(BaseModel): ...
 class CDESet(BaseModel):
     """Represents a CDE Set with its component Elements."""
 
-    id: str = Field(..., regex="^(RDES|TO_BE_DETERMINED)\d+", description='Must be a valid ID')
-    name: str = Field(..., max_length=50, description='Must be 50 or fewer characters long')
-    description: str = Field(..., max_length=100, description='Must be 100 or fewer characters long')
+    id: str = Field(..., pattern="^(RDES|TO_BE_DETERMINED)\d+", description="Must be a valid ID")
+    name: str = Field(..., max_length=50, description="Must be 50 or fewer characters long")
+    description: str = Field(..., max_length=100, description="Must be 100 or fewer characters long")
     version: Version
     status: Status
     # url field is a string containing a URL
-    url: HttpUrl # str = Field(..., regex="^https?://") - can do it this way or use pydantic built-in HttpUrl class that does the same thing
+    url: HttpUrl  # str = Field(..., regex="^https?://") - can do it this way or use pydantic built-in HttpUrl class that does the same thing
     index_codes: Array[IndexCode]
     body_parts: Array[BodyPart]
     authors: List[Union[Person, Organization]]
@@ -100,11 +109,12 @@ class CDESet(BaseModel):
     elements: Array[CDEElement]
     references: Array[Reference]
 
-#question about code above, the RSNA data type or most of these is an 'array': it can be coded as: index_codes: List[IndexCode] and still return an array in pydantic.
+
+# question about code above, the RSNA data type or most of these is an 'array': it can be coded as: index_codes: List[IndexCode] and still return an array in pydantic.
 # however, you can also import Array from typing and use it as shown above.
 # you can also use a pydantic library called 'pydantic-numpy' and use it to convert the array to a numpy array. But I am guessing we don't want to do that because
 # it would convert the array to the default numpy data type which is float. Correct?
-    # I assume we want to keep the data type as an array of strings, so we can use the array data type from the typing library or the list method? -Adam
+# I assume we want to keep the data type as an array of strings, so we can use the array data type from the typing library or the list method? -Adam
 
 
 class CDESetProcessor:
@@ -112,12 +122,11 @@ class CDESetProcessor:
         self.data = data
         self.index_codes = [IndexCode(**code) for code in data.index_codes]
         self.body_parts = [BodyPart(**part) for part in data.body_parts]
-        self.authors = [Person(**author) if 'person' in author else Organization(**author) for author in data.authors]
+        self.authors = [Person(**author) if "person" in author else Organization(**author) for author in data.authors]
         self.elements = [CDEElement(**element) for element in data.elements]
 
-
     @staticmethod
-    def fetch_from_repo(rdes_id: str) -> Optional['CDESetProcessor']:
+    def fetch_from_repo(rdes_id: str) -> Optional["CDESetProcessor"]:
         rad_element_api = f"https://api3.rsna.org/radelement/v1/sets/{rdes_id}"
         response = requests.get(rad_element_api)
         if response.status_code != 200:
@@ -126,7 +135,6 @@ class CDESetProcessor:
         json_data = response.json()
         cde_set_data = CDESet.model_validate_json(json_data)
         return CDESetProcessor(cde_set_data)
-
 
     @property
     def id(self):
