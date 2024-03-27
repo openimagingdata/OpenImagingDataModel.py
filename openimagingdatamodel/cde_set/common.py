bc0fc03ed8b90ecb0ebcb1149096a483042458a9
Tarik Alkasab <tarik@alkasab.org>
2024-03-27 15:47:00 -0400

    Reworking of CDE Set/Element definitions based on schema.

---
+++ openimagingdatamodel/cde_set/common.py
@@ -0,0 +1,79 @@
+from typing import Literal
+
+from pydantic import BaseModel, Field, HttpUrl
+
+
+class Specialty(BaseModel):
+    abbreviation: Literal[
+        "AB", "BR", "CA", "CH", "ER", "GI", "GU", "HN", "IR", "MI", "MK", "NR", "OB", "OI", "OT", "PD", "QI", "RS", "VA"
+    ]
+    name: str
+
+
+class Version(BaseModel):
+    number: int  # TODO: Minimum 1
+    date: str  # TODO: Add date format
+
+
+class Status(BaseModel):
+    date: str  # TODO: Add date format
+    name: Literal["Proposed", "Published", "Retired"]
+
+
+class Event(BaseModel):
+    date: str  # TODO: Add date format
+    status: Status
+
+
+class Organization(BaseModel):
+    name: str
+    url: HttpUrl | None = None
+    abbreviation: str | None = None
+    comment: str | None = None
+    role: Literal["author", "sponsor", "translator", "reviewer", "contributor"] | None = None
+
+
+class Person(BaseModel):
+    name: str
+    email: str  # TODO: Add email format
+    affiliation: str | None = None
+    orcid_id: str | None = None
+    twitter_handle: str | None = None
+    url: HttpUrl | None = None
+    role: Literal["Author", "Editor", "Translator", "Reviewer"] | None = None
+
+
+class Contributors(BaseModel):
+    people: list[Person] = Field(default_factory=list)
+    organizations: list[Organization] = Field(default_factory=list)
+
+
+class Reference(BaseModel):
+    citation: str = Field(
+        description="Required - Provide a bibliographic citation, including all the author names (no et Al)"
+    )
+    doi_uri: str | None = None  # TODO: Add refex for doi uri
+    pubmed_id: str | None = None  # TODO: Add refex for pubmed id
+    url: HttpUrl | None = None
+
+
+class IndexCode(BaseModel):
+    system: Literal["RADLEX", "SNOMED", "LOINC", "ACRCOMMON"] | None = None
+    code: str | None = None
+    display: str | None = None
+    url: HttpUrl | None = None
+
+
+class BodyPart(BaseModel):
+    name: str
+    index_codes: list[IndexCode] | None = None
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    height: int | None = None
+    width: int | None = None
+    caption: str | None = None
+    rights: str | None = None
+    contributors: Contributors | None = None
+    references: list[Reference] | None = None
---
+++ openimagingdatamodel/cde_set/set.py
@@ -0,0 +1,30 @@
+"""Represents a CDE Set with its component Elements."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pydantic import BaseModel, Field, HttpUrl
+
+if TYPE_CHECKING:
+    from .common import BodyPart, Contributors, Event, IndexCode, Reference, Specialty, Status, Version
+    from .element import CDEElement
+
+
+# https://github.com/RSNA/ACR-RSNA-CDEs/blob/master/cde.schema.json
+class CDESet(BaseModel):
+    """Represents a CDE Set with its component Elements."""
+
+    id: str = Field(..., pattern="^(RDES|TO_BE_DETERMINED)\d+", description="Must be a valid ID")
+    name: str = Field(..., max_length=50, description="Must be 50 or fewer characters long")
+    description: str = Field(..., max_length=100, description="Must be 100 or fewer characters long")
+    version: Version
+    status: Status
+    url: HttpUrl | None = None
+    index_codes: list[IndexCode] = Field(default_factory=list)
+    body_parts: list[BodyPart] | None = None
+    contributors: Contributors | None = None
+    history: list[Event] = Field(default_factory=list)
+    specialties: list[Specialty] = Field(default_factory=list)
+    elements: list[CDEElement] = Field(default_factory=list)  # TODO: Require at least one element
+    references: list[Reference] | None = None
