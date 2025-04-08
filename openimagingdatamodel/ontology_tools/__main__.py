f92fb842bbf3f0794de6b0fc4c868c9fd72433f4
Tarik Alkasab <tarik@alkasab.org>
2025-04-08 16:38:49 -0400

    Implement search to use LanceDB hybrid search.

--- openimagingdatamodel/ontology_tools/__main__.py
+++ openimagingdatamodel/ontology_tools/__main__.py
@@ -1,75 +1,29 @@
 import asyncio
-import sys
-import time
 from pprint import pprint
 
-from .config import Config
-from .search import SEARCH_TYPES, async_search  # , search
+from .config import settings
+from .search import llm_filter_results, search_one
 
 
-def embed(*args):
-    if len(args) != 3:
-        print("Please give the concept name, the total number of concepts, and the batch size.")
-        sys.exit(1)
-    concept_name, total_concepts, batch_size = args[0], int(args[1]), int(args[2])
-    if concept_name not in ["snomedct", "radlex", "anatomic_locations"]:
-        print("Invalid concept name. Please use one of: 'snomedct', 'radlex', 'anatomic_locations'.")
-        sys.exit(1)
+async def main():
+    search_results = []
+    for ontology in settings.known_ontologies:
+        repository = settings.get_repository(ontology)
+        count  = await repository.get_count()
+        print(f"Ontology: {ontology}: {count} items")
+        results = await search_one(ontology, "brain")
+        search_results.extend(results)
+        print(f" - {ontology}: {len(results)} results")
+    
+    print("Filtering results...")
+    targeted_results, related_results = await llm_filter_results(search_results, "brain")
+    print("Targeted results:")
+    pprint(targeted_results)
+    print("Related results:")
+    pprint(related_results)
 
-    config = Config()
-    embedding_creator = config.embedder
-    repo = config.get_repository(concept_name)
-
-    concepts = repo.get_unembedded_concepts(total_concepts)
-    print(f"Found {len(concepts)} unembedded concepts.")
-    total_start_time = time.time()
-    for i in range(0, len(concepts), batch_size):
-        batch = concepts[i : i + batch_size]
-        vectors = embedding_creator.create_embeddings_for_concepts(batch)
-        if len(vectors) != len(batch) or not all(len(v) == 1536 for v in vectors):
-            print("Error: Some vectors are missing or have the wrong length.")
-            sys.exit(1)
-        start_time = time.time()
-        result = repo.write_embedding_vectors(batch, vectors)
-        batch_time = time.time() - start_time
-        if result:
-            progress = f"{i + 1} - {i + batch_size} / {total_concepts}"
-            print(f"Successfully embedded {len(batch)} concepts ({progress}; {batch_time:.2f}s).")
-        else:
-            print("Error: Failed to write embedding vectors.")
-            sys.exit(1)
-    print(f"Total time: {time.time() - total_start_time:.2f}s")
 
 
 if __name__ == "__main__":
     print("OIDM Ontology Tools")
-    # See if the first command line argument is one of the commands: "search" or "embed"
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "search":
-            if len(sys.argv) < 6:
-                print("Please provide an ontology, a search type, a count, and a query.")
-                sys.exit(1)
-            config = Config()
-            if (ontology := sys.argv[2]) not in config.known_ontologies and ontology != "all":
-                print("Invalid ontology. Please use one of: 'snomedct', 'radlex', 'anatomic_locations'.")
-                sys.exit(1)
-            if (search_type := sys.argv[3]) not in SEARCH_TYPES:
-                print("Invalid search type. Please use one of: 'text', 'vector', 'all'.")
-                sys.exit(1)
-            if not (count := int(sys.argv[4].strip())) > 0:
-                print("Invalid count. Please provide a positive integer.")
-                sys.exit(1)
-            query = " ".join(sys.argv[5:])
-            start_time = time.time()
-            print("Searching...")
-            # results = search(config, ontology, search_type, query, count)
-            results = asyncio.run(async_search(config, ontology, search_type, query, count))
-            for result in results:
-                pprint(result.model_dump())
-            print(f"Search time: {time.time() - start_time:.2f}s")
-        elif sys.argv[1] == "embed":
-            embed(*sys.argv[2:])
-        else:
-            print("Invalid command. Please use either 'search' or 'embed'.")
-    else:
-        print("Available commands: 'search', 'embed'")
+    asyncio.run(main())
--- openimagingdatamodel/ontology_tools/search.py
+++ openimagingdatamodel/ontology_tools/search.py
@@ -1,139 +1,44 @@
-import asyncio
 from typing import Literal
 
 import instructor
 from openai import OpenAI
 from pydantic import BaseModel
 
-from . import repository
-from .config import Config
-from .embedding_creator import EmbeddingCreator
+from .config import settings
 from .search_result import SearchResult
 
-SEARCH_TYPES = ("text", "vector", "all")
 
+async def search_one(system: str, query: str, count: int = 20) -> list[SearchResult]:
+    db_conn = await settings.lancedb_conn
+    table = await db_conn.open_table(system.lower())
+    query_obj = await table.search(query, query_type="hybrid", vector_column_name="vector", fts_columns="concept_text")
+    raw_results = await (
+        query_obj.select(["concept_id", "concept_text"])
+        .limit(count)
+        .to_list()
+    )
 
-def normalize_results(results: list[SearchResult]) -> list[SearchResult]:
-    min_score = min(result.score for result in results)
-    max_score = max(result.score for result in results)
-    score_range = max_score - min_score
-    return [
+    # r["concept_id"], r["concept_text"], r["_relevance_score"]
+    results = [
         SearchResult(
-            system=result.system,
-            code=result.code,
-            display=result.display,
-            score=(result.score - min_score) / score_range,
+            system=system.upper(),
+            code=result["concept_id"],
+            display=result["concept_text"],
+            score=result["_relevance_score"],
         )
-        for result in results
+        for result in raw_results
     ]
-
-
-def search_one(
-    repo: repository.Repository,
-    search_type: str,
-    query: str,
-    count: int,
-    embedder: EmbeddingCreator | None = None,
-    normalize: bool = True,
-) -> list[SearchResult]:
-    assert search_type in SEARCH_TYPES, f"Invalid search type: {search_type}"
-    results = []
-    if search_type in ["vector", "all"]:
-        if embedder is None:
-            raise ValueError("Embedder required for vector search.")
-        query_vector = embedder.create_embedding_for_text(query)
-        vector_results = repo.vector_search(query_vector, count)
-        if normalize:
-            vector_results = normalize_results(vector_results)
-        results.extend(vector_results)
-    if search_type in ["text", "all"]:
-        text_results = repo.text_search(query, count)
-        if normalize:
-            text_results = normalize_results(text_results)
-        results.extend(text_results)
     return results
 
 
-async def async_search_one(
-    repo: repository.AsyncRepository,
-    search_type: str,
-    query: str,
-    count: int,
-    embedder: EmbeddingCreator | None = None,
-    normalize: bool = True,
-) -> list[SearchResult]:
-    assert embedder is not None or search_type not in ["vector", "all"], "Embedder required for vector search."
-    match search_type:
-        case "all":
-            query_vector = embedder.create_embedding_for_text(query)
-            vector_task = repo.vector_search(query_vector, count)
-            text_task = repo.text_search(query, count)
-            vector_results, text_results = await asyncio.gather(vector_task, text_task)
-            if normalize:
-                vector_results = normalize_results(vector_results)
-                text_results = normalize_results(text_results)
-            return vector_results + text_results
-        case "vector":
-            query_vector = embedder.create_embedding_for_text(query)
-            vector_results = await repo.vector_search(query_vector, count)
-            return normalize_results(vector_results) if normalize else vector_results
-        case "text":
-            text_results = await repo.text_search(query, count)
-            return normalize_results(text_results) if normalize else text_results
-        case _:
-            raise ValueError(f"Invalid search type: {search_type}")
-
-
-def search(
-    config: Config, ontology: str, search_type: str, query: str, count: int = 25, normalize: bool = True
-) -> list[SearchResult]:
-    if search_type not in SEARCH_TYPES:
-        raise ValueError(f"Invalid search type: {search_type}")
-    if ontology not in config.known_ontologies and ontology != "all":
-        raise ValueError(f"Invalid ontology: {ontology}")
-    if not (query := query.strip()) or len(query) < 3:
-        raise ValueError("Query must not be empty or less than 3 characters.")
-
-    embedder = config.embedder if search_type in ["vector", "all"] else None
-
-    if ontology == "all":
-        results = []
-        for ontology in config.known_ontologies:
-            repo = config.get_repository(ontology)
-            results.extend(search_one(repo, search_type, query, count, embedder))
-        return results
-
-    repo = config.get_repository(ontology)
-    results = search_one(repo, search_type, query, count, embedder)
-    if normalize:
-        results = normalize_results(results)
-    return results
-
-
-async def async_search(
-    config: Config, ontology: str, search_type: str, query: str, count: int = 25, normalize: bool = True
-) -> list[SearchResult]:
-    if search_type not in SEARCH_TYPES:
-        raise ValueError(f"Invalid search type: {search_type}")
-    if ontology not in config.known_ontologies and ontology != "all":
-        raise ValueError(f"Invalid ontology: {ontology}")
-    if not (query := query.strip()) or len(query) < 3:
-        raise ValueError("Query must not be empty or less than 3 characters.")
-
-    embedder = config.embedder if search_type in ["vector", "all"] else None
-
-    if ontology == "all":
-        tasks = [
-            async_search_one(config.get_async_repository(ontology), search_type, query, count, embedder)
-            for ontology in config.known_ontologies
-        ]
-        ontology_results: list[list[SearchResult]] = await asyncio.gather(*tasks)
-        return [result for results in ontology_results for result in results]
-
-    results = await async_search_one(config.get_async_repository(ontology), search_type, query, count, embedder)
-    if normalize:
-        results = normalize_results(results)
-    return results
+async def search(ontology: str, query: str, count: int = 25) -> list[SearchResult]:
+    # if search_type not in SEARCH_TYPES:
+    #     raise ValueError(f"Invalid search type: {search_type}")
+    # if ontology not in config.known_ontologies and ontology != "all":
+    #     raise ValueError(f"Invalid ontology: {ontology}")
+    # if not (query := query.strip()) or len(query) < 3:
+    #     raise ValueError("Query must not be empty or less than 3 characters.")
+    ...
 
 
 class SelectedResult(BaseModel):
@@ -142,12 +47,10 @@ class SelectedResult(BaseModel):
     display: str
 
 
-def llm_filter_results(
-    llm: OpenAI, results: list[SearchResult], query: str
-) -> tuple[list[dict[str, str]], list[dict[str, str]]]:
+async def llm_filter_results(results: list[SearchResult], query: str) -> tuple[list[dict[str, str]], list[dict[str, str]]]:
     query = query.strip()
 
-    instructor_llm = instructor.from_openai(llm)
+    instructor_llm = instructor.from_openai(settings.llm)
 
     prompt = f"Filter the following results based whether these are actual matches for the query: '{query}'\n\n"
     for result in results:
@@ -159,21 +62,20 @@ def llm_filter_results(
         + " ontologies are identical or related to a given query.",
     }
 
-    filtered_results = instructor_llm.chat.completions.create(
-        model="gpt-4o",
+    filtered_results = await instructor_llm.chat.completions.create(
+        model="gpt-4o-mini",
         response_model=list[SelectedResult],
         messages=[system_message, {"role": "user", "content": prompt}],  # type: ignore
     )
 
-    prompt = f"Filter the following results based whether these are not matches, but rather concepts related to the query: '{query}'\n\n"
+    prompt = f"Filter the following results based whether these are not matches, but rather concepts related to the query: '{query}'\n\n"  # noqa: E501
     for result in results:
         prompt += f"{result.short_string()}\n"
 
-    related_results = instructor_llm.chat.completions.create(
+    related_results = await instructor_llm.chat.completions.create(
         model="gpt-4o",
         response_model=list[SelectedResult],
         messages=[system_message, {"role": "user", "content": prompt}],  # type: ignore
     )
 
-    return ([result.model_dump() for result in filtered_results], 
-        [result.model_dump() for result in related_results])
+    return ([result.model_dump() for result in filtered_results], [result.model_dump() for result in related_results])
